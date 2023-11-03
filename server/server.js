1ea45ba6f89ceb424ebfa901df24fe7e8bac6ae6
Luke Trenaman <trenamanluke@gmail.com>
2023-11-03 17:38:00 -0400

    feat: create hello world between server and client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const cors = require("cors");
 require("dotenv").config()
 
 const app = express() 
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')

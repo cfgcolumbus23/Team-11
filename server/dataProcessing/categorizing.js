5dc6892da80fc8db83b930f450d0ff3a3603ed06
tiyachhajed <tiyaschhajed@gmail.com>
2023-11-03 21:44:15 -0400

    data processing in progress
    Decided cutoffs and how to separate data. In the process of organizing data into buckets and giving a rating and recommendations for each bucket

---
+++ server/dataProcessing/categorizing.js
@@ -0,0 +1,8 @@
+const physical_total = array[5] + array[8];
+const social_total = array[0] + array[7] + array[2];
+const math_total = array[9] + array[6];
+const reading_total = array[1] + array[3] + array[4] + array[10] + array[11];
+
+if (physical_total) {
+
+}

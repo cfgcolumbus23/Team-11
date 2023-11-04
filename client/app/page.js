c81bf283b664c63c5d85ea82702026b85944c867
amysarle <sarle.amy@gmail.com>
2023-11-04 04:22:50 -0400

    Changed link order

--- client/app/page.js
+++ client/app/page.js
@@ -4,18 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link
-          href="assessment"
-          className="text-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Assessment{' '}
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Fill out a form for a student to calculate relevant scores.
-          </p>
-        </Link>
 
         <Link
           href="students"
@@ -27,6 +15,19 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             View the students in your class
+          </p>
+        </Link>
+
+        <Link
+          href="assessment"
+          className="text-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Assessment{' '}
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Fill out a form for a student to calculate relevant scores.
           </p>
         </Link>
 

96683455ff03e54025112aa04ad77dad3f54a64d
cadeci <ciaracade03@gmail.com>
2023-11-03 21:23:18 -0400

    Working get all students post

--- server/routes/studentRouter.js
+++ server/routes/studentRouter.js
@@ -2,9 +2,16 @@ const express = require("express")
 const {
     makeStudent
 } = require("../controllers/student_controllers")
+const {
+    getStudents
+} = require("../controllers/student_controllers")
 
 const router = express.Router()
 
 router.post("/", makeStudent)
+
+
+router.get("/", getStudents)
+
 
 module.exports = router
